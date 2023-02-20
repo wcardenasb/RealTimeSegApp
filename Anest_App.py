fe010c8540690a3be10725f0d6bc3dfe05204938
wcardenasb <125897481+wcardenasb@users.noreply.github.com>
2023-02-20 14:13:19 -0500

    Update Anest_App.py

--- Anest_App.py
+++ Anest_App.py
@@ -9,7 +9,7 @@ from unet import unet
 from proceso import (imagenProceso, removerAreas, aumentoTam, cuadrarRect,
                      dimRec)
 
-from streamlit_webrtc import webrtc_streamer
+import streamlit_webrtc as webrtc
 
 # webrtc_streamer(key="example")
 
